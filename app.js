67010b4cd671c6626429c66539e761e2cd546d5d
Wissam Charafeddine <wissam.charafeddine@gmail.com>
2016-04-14 19:52:31 -0400

    adding a link to ngRoute

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('addToDoList', []);
+var app = angular.module('addToDoList', [ngRoute]);
 // This is declaring a module. More on this in a moment
   app.controller('ToDoListCtrl', function($scope){ $scope.list = ['eg. Pay DTE Bill'];
   $scope.add=  function(){
